3faf6472b44b9274d6394808af4c257156a92962
ImFl0wow <1720766910@qq.com>
2024-01-29 15:40:41 +0800

    added a test and fixed a random utils index bug

--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/utils/RandomUtils.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/utils/RandomUtils.java
@@ -57,7 +57,7 @@ public class RandomUtils {
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            int number = random.nextInt(62);
+            int number = random.nextInt(str.length());
             sb.append(str.charAt(number));
         }
         return sb.toString();
--- phantomshield-obfuscator/src/test/java/TestRun.java
+++ phantomshield-obfuscator/src/test/java/TestRun.java
@@ -6,8 +6,10 @@ import java.io.File;
 public class TestRun {
     public static void main(String[] args) {
         PhantomShield obfuscator = new PhantomShield(new ConfigBuilder()
-                .setInputJar(new File("asm-9.6.jar"))
-                .setOutputJar(new File("output\\asm-9.6.jar"))
+                .setInputJar(new File("test\\input\\obf-test-1.0-SNAPSHOT.jar"))
+                .setOutputJar(new File("test\\output\\obf-test-1.0-SNAPSHOT.jar"))
+                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "rt.jar")
+                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "jce.jar")
                 .setModeInvokeDynamicNativeConverter("enhancement")
                 .build());
         obfuscator.process();
