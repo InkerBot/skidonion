b00cadb37bf3260f926faf92a490e0c1d41abb33
ImFl0wow <1720766910@qq.com>
2024-02-12 18:25:21 +0800

    added a simple construction to StringArrayValue

--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/value/impls/StringArrayValue.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/value/impls/StringArrayValue.java
@@ -3,10 +3,15 @@ package tech.skidonion.obfuscator.value.impls;
 import com.google.gson.JsonElement;
 import tech.skidonion.obfuscator.value.Value;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringArrayValue extends Value<List<String>> {
+    public StringArrayValue(String name) {
+        this(name, Collections.emptyList());
+    }
+
     public StringArrayValue(String name, List<String> defaultValue) {
         super(name, defaultValue);
     }
