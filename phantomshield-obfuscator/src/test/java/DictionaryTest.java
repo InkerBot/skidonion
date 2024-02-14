5b14f19a59dbf46d040d95b02d2b603c83206c24
ImFl0wow <1720766910@qq.com>
2024-02-14 15:55:53 +0800

    ArrayList is shit

--- phantomshield-obfuscator/src/test/java/DictionaryTest.java
+++ phantomshield-obfuscator/src/test/java/DictionaryTest.java
@@ -3,27 +3,24 @@ import org.junit.jupiter.api.Test;
 import tech.skidonion.obfuscator.dictionary.Dictionary;
 import tech.skidonion.obfuscator.dictionary.DictionaryFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DictionaryTest {
-    private List<String> cached;
+    private Set<String> cached;
 
     @BeforeEach
     void setUp() {
-        cached = new ArrayList<>();
+        cached = new HashSet<>();
     }
 
     @Test
     void testGenerated() {
         Dictionary dictionary = DictionaryFactory.get("spaces");
-        for (int index = 0; index < 10000; index++) {
+        for (int index = 0; index < 100000; index++) {
             String generated = dictionary.nextUniqueString();
-            if (cached.contains(generated)) {
+            if (!cached.add(generated)) {
                 throw new RuntimeException("unique?");
-            } else {
-                cached.add(generated);
-                System.out.println(generated);
             }
         }
     }
