0552f460e67fa1ae5f270b1d2dd8d576e8cf5634
ImFl0wow <1720766910@qq.com>
2024-02-29 14:20:27 +0800

    improved debug run config

--- phantomshield-obfuscator/src/test/java/TestRun.java
+++ phantomshield-obfuscator/src/test/java/TestRun.java
@@ -1,43 +1,78 @@
 import tech.skidonion.obfuscator.PhantomShield;
+import tech.skidonion.obfuscator.config.Config;
 import tech.skidonion.obfuscator.config.ConfigBuilder;
 
 import java.io.File;
 
 public class TestRun {
     public static void main(String[] args) {
-        PhantomShield obfuscator = new PhantomShield(new ConfigBuilder()
-                .setInputJar(new File("test\\input\\obf-test-1.0-SNAPSHOT.jar"))
-                .setOutputJar(new File("test\\output\\obf-test-1.0-SNAPSHOT.jar"))
-                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "rt.jar")
-                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "jce.jar")
-                .setControlFlowObfuscationEnable(true) // control flow
-//                .addSubFilters("control_flow_obfuscation",
-//                        "+dev/sim0n/app/test/impl/flow/**",
-//                        "+dev/sim0n/app/test/impl/flow/** * *(*)")
-                .addSubFilters("control_flow_obfuscation",
-                        "+dev/sim0n/app/test/impl/crypttest/Blowfish",
-                        "+dev/sim0n/app/test/impl/crypttest/Blowfish * *(*)")
-//                .setInputMappingsFileSetting("mappings.json")
-                .setDebugInformationRemoverEnable(false) // Remover
-                .setMemberShufflerEnable(false) // Shuffler
-                .setRenamerEnable(false) // Renamer
-                .addAdaptResources("META-INF/MANIFEST.MF")
-                .setRepackageSetting(false)
-                .setPrintMappingsSetting(false)
-//                .setPrintMappingsFileSetting("mappings.json")
-//                .setPrefixName("狼牙")
-                .setRepackageNameSetting("skidonion")
-                .setStringEncryptionEnable(false) // String
-                .setNativeObfuscationEnable(false) // Native
-                .setPrintInstructionsSetting(false)
-                .setInvokeWrapperEnable(false) // InvokeWrapper
-                .addTarget("x86_64-windows")
-//                .addSubFilters("native_obfuscation",
-//                        "+dev/sim0n/app/test/impl/evaluation/EvaluationTest",
-//                        "+dev/sim0n/app/test/impl/evaluation/EvaluationTest * **(**)")
-
-                .build());
-        obfuscator.process();
+        ConfigBuilder builder = basic();
+//         =================
+//        debug_information_remover(builder);
+//        shuffler(builder);
+//        renamer(builder);
+//        string_encryption(builder);
+        invoke_wrapper(builder);
+//        control_flow(builder);
+//        native_obfuscation(builder);
+//         =================
+        new PhantomShield(builder.build()).process();
         System.exit(0);
+    }
+
+    private static void control_flow(ConfigBuilder builder) {
+        builder.setControlFlowObfuscationEnable(true)
+//                .addSubFilters("control_flow_obfuscation", //
+//                        "+dev/sim0n/app/test/impl/flow/**", //
+//                        "+dev/sim0n/app/test/impl/flow/** * *(*)") //
+                .addSubFilters("control_flow_obfuscation", //
+                        "+dev/sim0n/app/test/impl/crypttest/Blowfish", //
+                        "+dev/sim0n/app/test/impl/crypttest/Blowfish * *(*)"); //
+    }
+
+    private static void debug_information_remover(ConfigBuilder builder) {
+        builder.setDebugInformationRemoverEnable(true);
+    }
+
+    private static void shuffler(ConfigBuilder builder) {
+        builder.setMemberShufflerEnable(true);
+    }
+
+    private static void renamer(ConfigBuilder builder) {
+        builder.setRenamerEnable(true) //
+                .setRepackageSetting(false) //
+                .setRepackageNameSetting("skidonion") //
+                .setPrefixNameSetting("狼牙") //
+                .setPrintMappingsSetting(false) //
+                .setPrintMappingsFileSetting("mappings.json") //
+                .setInputMappingsFileSetting("mappings.json") //
+                .addAdaptResources("META-INF/MANIFEST.MF");
+    }
+
+    private static void string_encryption(ConfigBuilder builder) {
+        builder.setStringEncryptionEnable(true);
+    }
+
+    private static void native_obfuscation(ConfigBuilder builder) {
+        builder.setNativeObfuscationEnable(false) //
+                .setPrintInstructionsSetting(false) //
+                .addTarget("x86_64-windows") //
+                .addSubFilters("native_obfuscation", //
+                        "+dev/sim0n/app/test/impl/evaluation/EvaluationTest", //
+                        "+dev/sim0n/app/test/impl/evaluation/EvaluationTest * **(**)");
+    }
+
+    private static void invoke_wrapper(ConfigBuilder builder) {
+        builder.setInvokeWrapperEnable(true) //
+                .setInjectToOtherClassSetting(true) //
+                .setPackageModeSetting("random_existed");
+    }
+
+    private static ConfigBuilder basic() {
+        return new ConfigBuilder() //
+                .setInputJar(new File("test\\input\\obf-test-1.0-SNAPSHOT.jar")) //
+                .setOutputJar(new File("test\\output\\obf-test-1.0-SNAPSHOT.jar")) //
+                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "rt.jar") //
+                .addLibrary(System.getProperty("java.home") + File.separator + "lib" + File.separator + "jce.jar");
     }
 }
