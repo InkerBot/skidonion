05bc6c2d7ef1da166245974f38cbe9ffc5487a22
ImFl0wow <1720766910@qq.com>
2024-03-11 13:05:06 +0800

    an advanced hwid check

---
+++ phantomshield-obfuscator/src/test/java/tech/skidonion/obfuscator/utils/MachineIDUtilsTest.java
@@ -0,0 +1,24 @@
+package tech.skidonion.obfuscator.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MachineIDUtilsTest {
+
+    @Test
+    void generate() {
+        for (int i = 0; i < 10; i++) {
+            Object[] array = new Object[3];
+            array[0] = -1;
+            array[1] = ThreadLocalRandom.current().nextInt();
+//            MachineIDUtils.generate(array);
+            array[2] = "ff01000001ff";
+            MachineIDUtils.check(array);
+            System.out.println((((long) array[0] >> 32) ^ (int) array[1]) & 0b1);
+        }
+    }
+}
