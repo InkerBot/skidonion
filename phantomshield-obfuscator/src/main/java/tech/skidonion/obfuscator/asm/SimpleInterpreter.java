0804b585303602f3f8960b40d385b62a2a033201
ImFl0wow <1720766910@qq.com>
2024-04-06 18:18:59 +0800

    fixed a bug while analysis the frames

--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/asm/SimpleInterpreter.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/asm/SimpleInterpreter.java
@@ -68,16 +68,16 @@ public class SimpleInterpreter extends BasicInterpreter {
         return super.newOperation(insn);
     }
 
-    @Override
-    public BasicValue binaryOperation(AbstractInsnNode insn, BasicValue value1, BasicValue value2) throws AnalyzerException {
-        if (insn.getOpcode() == Opcodes.AALOAD) {
-            Type arrayType = value1.getType();
-            if (arrayType != null && arrayType.getSort() == Type.ARRAY) {
-                return new BasicValue(arrayType.getElementType());
-            }
-        }
-        return super.binaryOperation(insn, value1, value2);
-    }
+//    @Override
+//    public BasicValue binaryOperation(AbstractInsnNode insn, BasicValue value1, BasicValue value2) throws AnalyzerException {
+//        if (insn.getOpcode() == Opcodes.AALOAD) {
+//            Type arrayType = value1.getType();
+//            if (arrayType != null && arrayType.getSort() == Type.ARRAY) {
+//                return new BasicValue(arrayType.getElementType());
+//            }
+//        }
+//        return super.binaryOperation(insn, value1, value2);
+//    }
 
 
     @Override
