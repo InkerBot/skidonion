f814ec850599733eaf6110c6ba1c82f4b7a6cd9f
ImFl0wow <1720766910@qq.com>
2024-04-08 19:38:39 +0800

    renamer: fixed a concurrent bug on java 9+

--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/utils/MapUtils.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/utils/MapUtils.java
@@ -8,21 +8,29 @@ import java.util.function.Function;
 
 public class MapUtils {
     public static <K, V> V computeIfPresent(Map<K, V> map, K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
-        if (Objects.requireNonNull(map).get(key) != null) {
-            V oldValue = map.get(key);
-            V newValue = remappingFunction.apply(key, oldValue);
-            if (newValue != null) map.put(key, newValue);
-            else map.remove(key);
+        V oldValue;
+        if ((oldValue = Objects.requireNonNull(map).get(key)) != null) {
+            V newValue;
+            if ((newValue = remappingFunction.apply(key, oldValue)) != null) {
+                map.put(key, newValue);
+                return newValue;
+            } else {
+                map.remove(key);
+                return null;
+            }
         }
         return null;
     }
 
     public static <K, V> V computeIfAbsent(Map<K, V> map, K key, Function<? super K, ? extends V> mappingFunction) {
-        if (map.get(key) == null) {
-            V newValue = mappingFunction.apply(key);
-            if (newValue != null)
+        V oldValue;
+        if ((oldValue = map.get(key)) == null) {
+            V newValue;
+            if ((newValue = mappingFunction.apply(key)) != null) {
                 map.put(key, newValue);
+                return newValue;
+            }
         }
-        return null;
+        return oldValue;
     }
 }
