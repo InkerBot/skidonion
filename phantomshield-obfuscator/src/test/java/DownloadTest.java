df69289f8f7c5a1bf53c4d95f5879f34d642f317
ImFl0wow <1720766910@qq.com>
2024-04-10 15:28:22 +0800

    added benchmark and cleaned code

--- phantomshield-obfuscator/src/test/java/DownloadTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import tech.skidonion.obfuscator.cpp.CompilerUpdater;
-
-public class DownloadTest {
-    @Test
-    void update() {
-        CompilerUpdater.updateCompiler();
-    }
-}
--- phantomshield-obfuscator/src/test/java/FloatTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class FloatTest {
-    @Test
-    void floatTest() {
-        System.out.println(0.1f + 0.6f);
-        System.out.println(0.7f);
-    }
-}
