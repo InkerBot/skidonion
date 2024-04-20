1e47c1910373263f70bc62f86b6ff497ed815303
Elysium <is7wilight@gmail.com>
2024-04-20 14:08:32 +0800

    修复一些问题

--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/gui/Gui.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/gui/Gui.java
@@ -18,7 +18,7 @@ public final class Gui {
 
     public static Gui INSTANCE = new Gui();
 
-    private static final boolean RELEASE = true;
+    private static final boolean RELEASE = false;
 
     private Screen screen;
 
--- phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/gui/rendering/screen/implement/InitialScreen.java
+++ phantomshield-obfuscator/src/main/java/tech/skidonion/obfuscator/gui/rendering/screen/implement/InitialScreen.java
@@ -3,7 +3,9 @@ package tech.skidonion.obfuscator.gui.rendering.screen.implement;
 import org.lwjgl.nanovg.NVGColor;
 import org.lwjgl.nanovg.NVGPaint;
 import tech.skidonion.obfuscator.gui.glfw.Window;
+import tech.skidonion.obfuscator.gui.rendering.font.manager.FontManager;
 import tech.skidonion.obfuscator.gui.rendering.objective2d.implement.Rectangle;
+import tech.skidonion.obfuscator.gui.rendering.objective2d.implement.Text;
 import tech.skidonion.obfuscator.gui.rendering.screen.Screen;
 import tech.skidonion.obfuscator.gui.utils.NvgColorUtil;
 
@@ -21,9 +23,6 @@ public class InitialScreen extends Screen {
     @Override
     public void draw(long nvgContext) {
 
-    }
-
-    private void drawBackground(long nvgContext) {
         final int width = Window.INSTANCE.getWindowWidth();
         final int height = Window.INSTANCE.getWindowHeight();
 
@@ -35,12 +34,15 @@ public class InitialScreen extends Screen {
         final NVGColor outerColor = NvgColorUtil.color(nvgContext, new Color(255, 255, 255));
 
         final NVGPaint paint = NVGPaint.calloc();
-        nvgFillPaint(nvgContext, nvgRadialGradient(nvgContext, width / 2F, height / 1.5F, 0, height, innerColor, outerColor, paint));
+        nvgFillPaint(nvgContext, nvgRadialGradient(nvgContext, width / 2F, height * .95F, height / 10F, height, innerColor, outerColor, paint));
 
         innerColor.free();
         outerColor.free();
         paint.free();
 
         nvgFill(nvgContext);
+
+        new Text(50, 50, "欢迎回来, 用户名", FontManager.NOTO_SANS_SC_MEDIUM, 40, NVG_ALIGN_LEFT | NVG_ALIGN_TOP, Color.BLACK).draw(nvgContext);
+        new Text(50, 100, "距离您的订阅到期还剩：dd", FontManager.NOTO_SANS_SC_MEDIUM, 15, NVG_ALIGN_LEFT | NVG_ALIGN_TOP, Color.BLACK).draw(nvgContext);
     }
 }
